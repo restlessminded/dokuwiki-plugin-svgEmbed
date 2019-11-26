7e8e7452d8ca8ff0c96fffd47f2bc34740c35889
Michael Bowers <52088098+restlessminded@users.noreply.github.com>
2019-11-25 18:42:10 -0600

    Add files via upload
    Fix for issue #2, potentially fixes issue #1 by setting information relevant to scaling for printing.

---
+++ lang/en/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * English language file
+ *
+ * @author  Michael Bowers <restlessmind@gmail.com>
+ */
+
+$lang['max_svg_width']      = 'Maximum width for SVG file size if no sizes are specified in the wiki page.';
+$lang['default_width']      = 'Default fall-through width of an SVG file.';
+$lang['default_height']     = 'Default fall-through height of an SVG file.';
+
+?>
