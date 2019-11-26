0b7d1e2d66c783ec27104c3d832ffa5ea219ed1d
Michael Bowers <52088098+restlessminded@users.noreply.github.com>
2019-11-25 18:41:25 -0600

    Add files via upload
    Fix for issue #2, potentially fixes issue #1 by setting information relevant to scaling for printing.

---
+++ conf/default.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Options for the svgembed plugin
+ *
+ * @author  Michael Bowers <restlessmind@gmail.com>
+ */
+
+$conf['max_svg_width']      = 1024;         // Maximum width of an SVG file
+$conf['default_width']      = 300;          // Default fall-through width of an SVG file
+$conf['default_height']     = 150;          // Default fall-through height of an SVG file
+
+?>
---
+++ conf/metadata.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Metadata for configuration manager plugin
+ * Additions for the svgembed plugin
+ *
+ * @author  Michael Bowers <restlessmind@gmail.com>
+ */
+
+$meta['max_svg_width']      = array('numeric', '_min' => 1);
+$meta['default_width']      = array('numeric', '_min' => 1);
+$meta['default_height']     = array('numeric', '_min' => 1);
+
+?>
