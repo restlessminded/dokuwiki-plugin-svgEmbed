afdb46d62d29cc384c45003e730a58830b1e4955
Michael Bowers <52088098+restlessminded@users.noreply.github.com>
2020-10-12 19:41:21 -0500

    Add files via upload
    Added text for new print configuration option

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -8,5 +8,6 @@
 $lang['max_svg_width']      = 'Maximum width for SVG file size if no sizes are specified in the wiki page.';
 $lang['default_width']      = 'Default fall-through width of an SVG file.';
 $lang['default_height']     = 'Default fall-through height of an SVG file.';
+$lang['default_print']      = 'All files embedded default to having a print button.';
 
 ?>
