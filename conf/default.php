d1a785c590d30e9f1deec3ecb20e9421484629a9
Michael Bowers <52088098+restlessminded@users.noreply.github.com>
2020-10-12 19:42:20 -0500

    Add files via upload
    Added optional SVG printing support

--- conf/default.php
+++ conf/default.php
@@ -8,5 +8,6 @@
 $conf['max_svg_width']      = 1024;         // Maximum width of an SVG file
 $conf['default_width']      = 300;          // Default fall-through width of an SVG file
 $conf['default_height']     = 150;          // Default fall-through height of an SVG file
+$conf['default_print']      = 0;            // Default value for printing configuration
 
 ?>
--- conf/metadata.php
+++ conf/metadata.php
@@ -9,5 +9,6 @@
 $meta['max_svg_width']      = array('numeric', '_min' => 1);
 $meta['default_width']      = array('numeric', '_min' => 1);
 $meta['default_height']     = array('numeric', '_min' => 1);
+$meta['default_print']      = array('onoff');
 
 ?>
