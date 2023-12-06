9c995163982b60d056f0d29afe8ae8e34d3f9c4a
Emma Bowers <52088098+restlessminded@users.noreply.github.com>
2023-12-06 14:19:24 -0600

    Update metadata.php
    Removing deadname

--- conf/metadata.php
+++ conf/metadata.php
@@ -3,7 +3,7 @@
  * Metadata for configuration manager plugin
  * Additions for the svgembed plugin
  *
- * @author  Michael Bowers <restlessmind@gmail.com>
+ * @author  Emma Bowers <restlessmind@gmail.com>
  */
 
 $meta['max_svg_width']      = array('numeric', '_min' => 1);
